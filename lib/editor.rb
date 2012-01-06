d60c8b0ab154dc63db708d74843bb01c8df87134
Larry Gilbert <larry@l2g.to>
2012-01-05 17:12:30 -0800

    Fleshed out documentation for Editor (also adjusted whitespace therein)

--- lib/editor.rb
+++ lib/editor.rb
@@ -1,7 +1,7 @@
 #!/usr/bin/env ruby
 
 # editor.rb
-# 
+#
 # Copyright (c) 2011, 2012 Lawrence Leonard Gilbert
 #
 # The SQL code (incorporated for documentation purposes) comes from the
@@ -43,27 +43,93 @@
 #     last_updated        TIMESTAMP WITH TIME ZONE DEFAULT NOW()
 # );
 
-#
+#==============================================================================
+# This model holds data for individual editors who are registered on the
+# MusicBrainz server.
+
 class Editor
     include DataMapper::Resource
-    
-    property :id,                  Serial
-    property :name,                String,   :length => 64, :required => true
-    property :password,            String,   :length => 64, :required => true
-    property :privs,               Flag,     :default => 0,
+
+    # @attribute
+    # @return [Integer]
+    property :id, Serial
+
+    # @attribute
+    # @return [String] Login name identifying the editor.
+    property :name, String, :length => 64, :required => true
+
+    # @attribute
+    # Holds the password used by the editor to log in. (Note that in the
+    # public data provided by MusicBrainz, this field is sanitized and does
+    # not hold any real passwords.)
+    # @return [String]
+    property :password, String, :length => 64, :required => true
+
+    # @attribute
+    # Flags indicating which editing privileges are given to the editor. Value
+    # is an array of zero or more of the following symbols:
+    # * +:account_admin+
+    # * +:auto_editor+
+    # * +:bot+
+    # * +:dont_nag+
+    # * +:mbid_submitter+
+    # * +:relationship_editor+
+    # * +:untrusted+
+    # * +:wiki_transclusion+
+    # @return [Array<Symbol>] Flags indicating editor's privileges
+    property :privs, Flag, :default => 0,
                 :flags => [:auto_editor, :bot, :untrusted,
                            :relationship_editor, :dont_nag,
                            :wiki_transclusion, :mbid_submitter,
                            :account_admin]
-    property :email,               String,   :length => 64
-    property :website,             URI,      :length => 255
-    property :bio,                 Text
-    property :member_since,        DateTime, :default => Time.now
-    property :email_confirm_date,  DateTime
-    property :last_login_date,     DateTime
-    property :edits_accepted,      Integer,  :default => 0
-    property :edits_rejected,      Integer,  :default => 0
-    property :auto_edits_accepted, Integer,  :default => 0
-    property :edits_failed,        Integer,  :default => 0
-    property :updated_at,          DateTime, :field => 'last_updated'
+
+    # @attribute
+    # @return [String, nil] Email address, if provided by the editor.
+    property :email, String, :length => 64
+
+    # @attribute
+    # @return [URI, nil] Website address, if provided by the editor.
+    property :website, URI, :length => 255
+
+    # @attribute
+    # @return [Text, nil] Biographical self-description, if provided by the
+    #   editor.
+    property :bio, Text
+
+    # @attribute
+    # @return [DateTime] When the editor registered this account.
+    property :member_since, DateTime, :default => Time.now
+
+    # @attribute
+    # @return [DateTime, nil] When the editor's email address was confirmed;
+    #   nil if not (yet) confirmed.
+    property :email_confirm_date, DateTime
+
+    # @attribute
+    # @return [DateTime, nil] When the editor last logged in to the server.
+    property :last_login_date, DateTime
+
+    # @attribute
+    # @return [Integer] Number of editor's edits that have been accepted
+    #   following a vote.
+    property :edits_accepted, Integer, :default => 0
+
+    # @attribute
+    # @return [Integer] Number of editor's edits that have been rejected by a
+    #   vote.
+    property :edits_rejected, Integer, :default => 0
+
+    # @attribute
+    # @return [Integer] Number of editor's edits that have been accepted
+    #   immediately by the server.
+    property :auto_edits_accepted, Integer, :default => 0
+
+    # @attribute
+    # @return [Integer] Number of editor's edits that have failed for reasons
+    #   other than being voted down (e.g. data dependency failures).
+    property :edits_failed, Integer, :default => 0
+
+    # @attribute
+    # @return [DateTime, nil] When this editor's account was last updated.
+    property :updated_at, DateTime, :field => 'last_updated'
 end
