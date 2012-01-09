faf21edc4fc7809a44a36112c518b28b01119e5f
Larry Gilbert <larry@l2g.to>
2012-01-08 16:53:03 -0800

    brainzmapper.rb is now a BrainzMapper class, but mainly just a convenience for setting up DataMapper with our models

--- lib/brainzmapper.rb
+++ lib/brainzmapper.rb
@@ -24,7 +24,7 @@
 
 ###############################################################################
 #
-# Brainzmapper is a DataMapper-based library for accessing a local copy of the
+# BrainzMapper is a DataMapper-based library for accessing a local copy of the
 # MusicBrainz database as Ruby objects.
 #
 ###############################################################################
@@ -107,10 +107,37 @@ require 'url'
 # All "advanced relationships"
 require 'ar'
 
-POSTGRES_URI = 'postgres://musicbrainz_rw@localhost/musicbrainz_db?search_path=musicbrainz'
+# Use +require 'brainzmapper'+ to load all of BrainzMapper's MusicBrainz
+# models. Then you can use the {BrainzMapper.setup} method to set up a
+# DataMapper connection and the correct naming convention for MusicBrainz, all
+# in one step (instead of two).
+#
+# @example
+#   require 'brainzmapper'
+#
+#   db_uri = 'postgres://musicbrainz_rw@localhost/musicbrainz_db?search_path=musicbrainz'
+#   BrainzMapper.setup(:default, db_uri)
+#
+#   # (...could do other DataMapper things here before finalizing...)
+#
+#   DataMapper.finalize
 
-DataMapper::Logger.new($stdout, :debug)
-ADAPTER = DataMapper.setup(:default, POSTGRES_URI)
-ADAPTER.resource_naming_convention =
-    DataMapper::NamingConventions::Resource::Underscored
+class BrainzMapper
+    NAMING_CONVENTION = DataMapper::NamingConventions::Resource::Underscored
 
+    # This method is for convenience of setting up DataMapper for use with the
+    # BrainzMapper classes.
+    #
+    # For the simplest case, see the example in Overview above. For more
+    # possibilities, see DataMapper's documentation for {DataMapper.setup}.
+    #
+    # You should still call {DataMapper.finalize} yourself!
+    #
+    # @return [DataMapper::Adapters::AbstractAdapter]
+
+    def BrainzMapper.setup(*args)
+        adapter = DataMapper.setup(*args)
+        adapter.resource_naming_convention = NAMING_CONVENTION
+        return adapter
+    end
+end
